aa64c2d5bee2cf97781bb9a003fb0045fb6531f2
PhuHVN <ngocphu2k4@gmail.com>
2025-01-22 22:31:34 +0700

    add repository file and code

---
+++ src/main/java/com/example/GrowChild/service/AuthenticationService.java
@@ -0,0 +1,19 @@
+package com.example.GrowChild.service;
+
+import com.example.GrowChild.entity.User;
+import com.example.GrowChild.repository.AuthenticationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthenticationService {
+    @Autowired
+    AuthenticationRepository authenticationRepository;
+
+    //getAllUser
+    public List<User> getUser(){
+        return authenticationRepository.findAll();
+    }
+}
