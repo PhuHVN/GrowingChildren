77500669a65c4bbffaef10c1d40b4e71754cb27c
PhuHVN <ngocphu2k4@gmail.com>
2025-01-23 19:35:18 +0700

    add file handle exception

---
+++ src/main/java/com/example/GrowChild/GlobalExceptionHandle/ExceptionHandle.java
@@ -0,0 +1,19 @@
+package com.example.GrowChild.GlobalExceptionHandle;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandle {
+    @ExceptionHandler(RuntimeException.class)
+    ResponseEntity handleRuntimeException(RuntimeException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    ResponseEntity handIllegalArgumentException(IllegalArgumentException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
