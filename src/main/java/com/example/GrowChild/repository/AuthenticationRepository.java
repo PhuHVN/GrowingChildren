b488b654d95579fc3fb07138eb3939b0b220c5f0
PhuHVN <ngocphu2k4@gmail.com>
2025-02-06 20:16:12 +0700

    add func interface

--- src/main/java/com/example/GrowChild/repository/AuthenticationRepository.java
+++ src/main/java/com/example/GrowChild/repository/AuthenticationRepository.java
@@ -4,7 +4,12 @@ package com.example.GrowChild.repository;
 import com.example.GrowChild.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthenticationRepository extends JpaRepository<User,String> {
     User findByUsername(String username);
     User findByEmail(String email);
+    List<User> findByRole_RoleName(String roleName);
+    List<User> findByRole_RoleId(long role_id);
+
 }
