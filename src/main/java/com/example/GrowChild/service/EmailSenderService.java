8506ce192a410431af799bd6b530e22b860257aa
PhuHVN <ngocphu2k4@gmail.com>
2025-02-12 19:36:31 +0700

    change name func login

--- src/main/java/com/example/GrowChild/service/EmailSenderService.java
+++ src/main/java/com/example/GrowChild/service/EmailSenderService.java
@@ -1,18 +1,13 @@
 package com.example.GrowChild.service;
 
-import com.example.GrowChild.entity.OTP;
-import com.example.GrowChild.repository.AuthenticationRepository;
+import com.example.GrowChild.repository.UserRepository;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 @Service
@@ -20,9 +15,7 @@ public class EmailSenderService {
     @Autowired
     private JavaMailSender mailSender;
     @Autowired
-    private AuthenticationRepository authenticationRepository;
-
-
+    private UserRepository userRepository;
 
     public String generateCode(){
         return String.valueOf((int)(Math.random() * 1000000)); // random 6 char number
