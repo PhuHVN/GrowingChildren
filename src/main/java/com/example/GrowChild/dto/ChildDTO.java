c5ff742ee5dad98be00860d25cdefb74aea7cd29
PhuHVN <ngocphu2k4@gmail.com>
2025-02-16 20:34:55 +0700

    add obj healthRecord

---
+++ src/main/java/com/example/GrowChild/dto/ChildDTO.java
@@ -0,0 +1,19 @@
+package com.example.GrowChild.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChildDTO {
+    private long childrenId;
+    private String childrenName;
+    private int age;
+    private String gender;
+    private String parentId;
+
+
+}
