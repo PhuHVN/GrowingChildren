0f8ff591b3ed20180b33e1ab19ffc1e69b9a218d
PhuHVN <ngocphu2k4@gmail.com>
2025-02-22 13:00:54 +0700

    add  scheduleDTO

---
+++ src/main/java/com/example/GrowChild/dto/ScheduleDTO.java
@@ -0,0 +1,20 @@
+package com.example.GrowChild.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScheduleDTO {
+    private long scheduleId;
+    private String doctorId;
+    private String doctorName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime scheduleWork;
+    private boolean isDelete;
+}
