a43dc0dfbbdac554dcda6020aa54823f6acfdef5
PhuHVN <ngocphu2k4@gmail.com>
2025-02-22 13:00:41 +0700

    add entity scheduleDoctor

---
+++ src/main/java/com/example/GrowChild/entity/ScheduleDoctor.java
@@ -0,0 +1,26 @@
+package com.example.GrowChild.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ScheduleDoctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long scheduleId;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id", nullable = false)
+    public User doctor;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    public LocalDateTime scheduleWork;
+    public boolean isDelete;
+}
