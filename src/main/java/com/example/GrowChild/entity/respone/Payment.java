b377585290aff966e7d3d992ee7fcb44337b78e0
PhuHVN <ngocphu2k4@gmail.com>
2025-02-28 10:25:23 +0700

    make entity

---
+++ src/main/java/com/example/GrowChild/entity/respone/Payment.java
@@ -0,0 +1,41 @@
+package com.example.GrowChild.entity.respone;
+
+
+import com.example.GrowChild.entity.enumStatus.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long paymentId;
+
+    @Enumerated(EnumType.STRING)
+    PaymentStatus status; //success or fail
+
+    String transactionId;
+
+    double price;
+
+    String paymentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+     User user; // Ai thực hiện thanh toán
+
+    @ManyToOne
+    @JoinColumn(name = "membership_id", nullable = false)
+     Membership membership; // Membership được mua
+
+}
