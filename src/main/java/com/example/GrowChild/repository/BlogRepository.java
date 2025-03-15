0da314d8e0e03c06df4dbac2d7eb91866c795ca0
Quan2906 <vipkocanhoi01@gmail.com>
2025-03-15 13:39:05 +0700

    update neww

--- src/main/java/com/example/GrowChild/repository/BlogRepository.java
+++ src/main/java/com/example/GrowChild/repository/BlogRepository.java
@@ -1,12 +1,15 @@
 package com.example.GrowChild.repository;
 
 import com.example.GrowChild.entity.response.Blog;
+import com.example.GrowChild.entity.response.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
     List<Blog> findBlogByIsDeleteFalse();
+
+    List<Blog> findByParentIdAndIsDeleteFalse(User parent);
 
     Blog findBlogByIsDeleteFalseAndBlogId(Long BlogId);
 }
