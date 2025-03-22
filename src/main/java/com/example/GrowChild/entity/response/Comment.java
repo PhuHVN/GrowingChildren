b1938e39ea9924aceaaeb120e8bfd40adbc5c445
Quan2906 <vipkocanhoi01@gmail.com>
2025-03-22 14:56:42 +0700

    update neww

--- src/main/java/com/example/GrowChild/entity/response/Comment.java
+++ src/main/java/com/example/GrowChild/entity/response/Comment.java
@@ -1,6 +1,8 @@
 package com.example.GrowChild.entity.response;
 
 
+import com.example.GrowChild.entity.enumStatus.BlogStatus;
+import com.example.GrowChild.entity.enumStatus.CommentStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
@@ -24,6 +26,10 @@ public class Comment {
     public boolean isDelete = false;
 
     @ManyToOne
+    @JoinColumn(name = "parent_id", nullable = false)
+    public User parentId;
+
+    @ManyToOne
     @JoinColumn(name = "blog_id", nullable = false)
     public Blog blogId;
 
@@ -33,4 +39,7 @@ public class Comment {
     protected void onCreate() {
         this.date = LocalDateTime.now();
     }
+
+    @Enumerated(EnumType.STRING)
+    private CommentStatus status;
 }
